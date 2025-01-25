34d76351bbec2e01fcd7dd4fe62398cd4002bb6e
Klaudia <klaud.subs@gmail.com>
2025-01-25 21:30:16 +0100

    'Two sum' task solution using Map

--- src/main/java/org/sawaklaudia/TwoSum.java
+++ src/main/java/org/sawaklaudia/TwoSum.java
@@ -32,9 +32,12 @@ public class TwoSum {
     public static int[] twoSumSolution3(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
+            if(map.containsKey(target - nums[i])) {
+                return new int[] {i, map.get(target - nums[i])};
+            } else {
+                map.put(nums[i], i);
+            }
         }
-        // todo
         return null;
     }
 
@@ -42,11 +45,11 @@ public class TwoSum {
         int[] nums = {2,7,11,15};
         int target = 9;
 
-        System.out.println(Arrays.toString(twoSum(nums, target)));
+        System.out.println(Arrays.toString(twoSumSolution3(nums, target)));
 
         nums = new int[]{3, 2, 4};
         target = 6;
 
-        System.out.println(Arrays.toString(twoSumSolution2(nums, target)));
+        System.out.println(Arrays.toString(twoSumSolution3(nums, target)));
     }
 }
