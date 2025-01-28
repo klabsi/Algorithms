7c2f7290f2ebd1dc6bc4d1d6f3943907db1b4129
Klaudia <klaud.subs@gmail.com>
2025-01-28 21:22:33 +0100

    Delete unused import statement

--- src/main/java/org/sawaklaudia/ContainsDuplicate.java
+++ src/main/java/org/sawaklaudia/ContainsDuplicate.java
@@ -1,7 +1,6 @@
 package org.sawaklaudia;
 
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 public class ContainsDuplicate {
@@ -10,7 +9,7 @@ public class ContainsDuplicate {
         Set<Integer> set = new HashSet<>();
 
         for (int num : nums) {
-            if(set.contains(num)) {
+            if (set.contains(num)) {
                 return true;
             } else {
                 set.add(num);
