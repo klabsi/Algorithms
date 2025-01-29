26da49cb403dcb969ad6065fbb2d7f709ca87b24
Klaudia <klaud.subs@gmail.com>
2025-01-29 23:20:15 +0100

    'Valid anagram' task solution #2

--- src/main/java/org/sawaklaudia/ValidAnagram.java
+++ src/main/java/org/sawaklaudia/ValidAnagram.java
@@ -12,7 +12,7 @@ public class ValidAnagram {
 
         Map<Character, Integer> mapS = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
-            mapS.put(s.charAt(i), mapS.getOrDefault(s.charAt(i), 0)+ 1);
+            mapS.put(s.charAt(i), mapS.getOrDefault(s.charAt(i), 0) + 1);
         }
 
         Map<Character, Integer> mapT = new HashMap<>();
@@ -20,11 +20,32 @@ public class ValidAnagram {
             if (!mapS.containsKey(t.charAt(i))) {
                 return false;
             } else {
-                mapT.put(t.charAt(i), mapT.getOrDefault(t.charAt(i), 0)+ 1);
+                mapT.put(t.charAt(i), mapT.getOrDefault(t.charAt(i), 0) + 1);
             }
         }
         System.out.println("S HashMap: " + mapS);
         System.out.println("T HashMap: " + mapT);
         return mapS.equals(mapT);
+    }
+
+    public boolean isAnagram2(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26]; // lowercase ASCII - 26 characters
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int c : count) {
+            if (c != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
