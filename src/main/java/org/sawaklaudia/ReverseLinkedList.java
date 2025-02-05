0cd06aadd76cd84a2d59c7a346cf1eae7637b2cf
Klaudia <klaud.subs@gmail.com>
2025-02-05 23:37:54 +0100

    'Reverse Linked List' task solution

--- src/main/java/org/sawaklaudia/ReverseLinkedList.java
+++ src/main/java/org/sawaklaudia/ReverseLinkedList.java
@@ -1,10 +1,25 @@
 package org.sawaklaudia;
 
 public class ReverseLinkedList {
-    public ListNode reverseList(ListNode head) {
 
+    public ListNode reverseListIteratively(ListNode head) {
+        ListNode previous = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
 
-        return head;
+        return previous;
+    }
+
+    public void printListNode(ListNode listNode) {
+        while (listNode != null) {
+            System.out.println(listNode.val);
+            listNode = listNode.next;
+        }
     }
 }
 
