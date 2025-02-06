deed8c702594691713aea7b756eeb853292bca0d
Klaudia <klaud.subs@gmail.com>
2025-02-06 16:09:03 +0100

    Override equals and hashCode methods in ListNode

--- src/main/java/org/sawaklaudia/ListNode.java
+++ src/main/java/org/sawaklaudia/ListNode.java
@@ -1,5 +1,7 @@
 package org.sawaklaudia;
 
+import java.util.Objects;
+
 public class ListNode {
 
     int val;
@@ -15,5 +17,23 @@ public class ListNode {
     public ListNode(int val, org.sawaklaudia.ListNode next) {
         this.val = val;
         this.next = next;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        org.sawaklaudia.ListNode listNode = (org.sawaklaudia.ListNode) o;
+
+        if (val != listNode.val) return false;
+        return Objects.equals(next, listNode.next);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = val;
+        result = 31 * result + (next != null ? next.hashCode() : 0);
+        return result;
     }
 }
