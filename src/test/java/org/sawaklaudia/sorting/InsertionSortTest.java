ddccdbcbcb728b9dc395a89bccd3b4b7a7020824
Klaudia <klaud.subs@gmail.com>
2025-03-20 11:35:59 +0100

    Test insertion sort algorithm.

---
+++ src/test/java/org/sawaklaudia/sorting/InsertionSortTest.java
@@ -0,0 +1,72 @@
+package org.sawaklaudia.sorting;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class InsertionSortTest {
+
+    private SortingAlgorithmI sortingAlgorithmI;
+
+    @BeforeEach
+    void setUp() {
+        sortingAlgorithmI = new InsertionSort();
+    }
+
+    @Test
+    void shouldSortArray() {
+        // given
+        int[] tab = {1, 2, 5, 4, 10, 10, -1, 5};
+
+        // when
+        int[] actual = sortingAlgorithmI.sort(tab);
+
+        // then
+        for (int i = 0; i < actual.length - 1; i++) {
+            Assertions.assertTrue(actual[i] <= actual[i + 1]);
+        }
+    }
+
+    @Test
+    void shouldSortArrayEx2() {
+        // given
+        int[] tab = {-5, 7, -2, -5, -100, 100, 52, 150};
+
+        // when
+        int[] actual = sortingAlgorithmI.sort(tab);
+
+        // then
+        for (int i = 0; i < actual.length - 1; i++) {
+            Assertions.assertTrue(actual[i] <= actual[i + 1]);
+        }
+    }
+
+    @Test
+    void shouldSortArrayEx3() {
+        // given
+        int[] tab = {5, 0, 0, 0, 1, -1};
+
+        // when
+        int[] actual = sortingAlgorithmI.sort(tab);
+
+        // then
+        for (int i = 0; i < actual.length - 1; i++) {
+            Assertions.assertTrue(actual[i] <= actual[i + 1]);
+        }
+    }
+
+    @Test
+    void shouldSortArrayEx4() {
+        // given
+        int[] tab = {0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0};
+
+        // when
+        int[] actual = sortingAlgorithmI.sort(tab);
+
+        // then
+        for (int i = 0; i < actual.length - 1; i++) {
+            Assertions.assertTrue(actual[i] <= actual[i + 1]);
+        }
+    }
+
+}
