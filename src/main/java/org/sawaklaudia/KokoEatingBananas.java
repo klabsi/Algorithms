344f09cdb66b4328d42d82fed32aca460fdf0bd6
Klaudia <klaud.subs@gmail.com>
2025-03-20 11:40:21 +0100

    Calculate Big O notation for the solution.

--- src/main/java/org/sawaklaudia/KokoEatingBananas.java
+++ src/main/java/org/sawaklaudia/KokoEatingBananas.java
@@ -5,20 +5,20 @@ public class KokoEatingBananas {
     public int minEatingSpeed(int[] piles, int h) {
         int actualSpeed = 0;
         int maxPile = 0;
-        for(int i = 0; i < piles.length; i++) {
-            if(piles[i] > maxPile){
+        for (int i = 0; i < piles.length; i++) {
+            if (piles[i] > maxPile) {
                 maxPile = piles[i];
             }
         }
         int left = 1;
         int right = maxPile;
 
-        while (left <= right){
+        while (left <= right) {
             int middle = (left + right) / 2;
-            for(int i = 0; i < piles.length; i++) {
-                actualSpeed += Math.ceil((double)piles[i] / (double)middle);
+            for (int i = 0; i < piles.length; i++) {
+                actualSpeed += Math.ceil((double) piles[i] / (double) middle);
             }
-            if(actualSpeed <= h) {
+            if (actualSpeed <= h) {
                 right = middle - 1;
             } else {
                 left = middle + 1;
@@ -28,3 +28,6 @@ public class KokoEatingBananas {
         return left;
     }
 }
+
+
+// n + log(n) * n
