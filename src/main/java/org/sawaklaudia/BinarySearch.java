201174406d768cddc0e5dd4f1bff4fb0bef8cbc9
Klaudia <klaud.subs@gmail.com>
2025-03-20 11:42:35 +0100

    Add other ways to solve the binary searching task.

--- src/main/java/org/sawaklaudia/BinarySearch.java
+++ src/main/java/org/sawaklaudia/BinarySearch.java
@@ -1,5 +1,8 @@
 package org.sawaklaudia;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class BinarySearch {
 
     public int search(int[] nums, int target) {
@@ -18,5 +21,38 @@ public class BinarySearch {
         }
 
         return -1;
+    }
+
+    public int searchWithForLoop(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] nums = new int[100_000];
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = random.nextInt(100);
+        }
+        Arrays.sort(nums);
+        int target = 66;
+
+        BinarySearch binarySearch = new BinarySearch();
+
+        long before = System.nanoTime();
+        binarySearch.searchWithForLoop(nums, target);
+        long after = System.nanoTime();
+        System.out.println("for " + (after - before));
+
+        before = System.nanoTime();
+        binarySearch.search(nums, target);
+        after = System.nanoTime();
+        System.out.println("binary " + (after - before));
+
     }
 }
