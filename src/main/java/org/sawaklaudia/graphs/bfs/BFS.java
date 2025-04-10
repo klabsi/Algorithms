388fc7fb2e56c54a38a88afc8fc9ea924f267515
Klaudia <klaud.subs@gmail.com>
2025-04-10 21:24:13 +0200

    Implement BFS algorithm.

---
+++ src/main/java/org/sawaklaudia/graphs/bfs/BFS.java
@@ -0,0 +1,42 @@
+package org.sawaklaudia.graphs.bfs;
+
+import org.sawaklaudia.graphs.dfs.Node;
+import org.sawaklaudia.graphs.dfs.Runner;
+
+import java.util.*;
+
+public class BFS {
+
+    public static void main(String[] args) {
+        System.out.println("tree");
+        bfs(Runner.tree());
+        System.out.println("\ngraph");
+        bfs(Runner.graph());
+    }
+
+    public static void  bfs(Node node) {
+        Queue<Node> queue = new ArrayDeque<>();
+        Set<Node> visited = new HashSet<>();
+        queue.offer(node);
+        bfs(queue, visited);
+    }
+
+    private static void bfs(Queue<Node> queue, Set<Node> visited) {
+        while (!queue.isEmpty()) {
+            Node first = queue.poll();
+            System.out.print(first.getValue() + " -> ");
+            if(first.getLeft() != null) {
+                if(!visited.contains(first.getLeft())) {
+                    visited.add(first.getLeft());
+                    queue.offer(first.getLeft());
+                }
+            }
+            if(first.getRight() != null) {
+                if(!visited.contains(first.getRight())) {
+                    visited.add(first.getRight());
+                    queue.offer(first.getRight());
+                }
+            }
+        }
+    }
+}
