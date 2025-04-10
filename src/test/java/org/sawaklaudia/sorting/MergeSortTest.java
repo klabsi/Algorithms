4ceac9d11f871ff60cfbfb27a288e58ea68c422e
Klaudia <klaud.subs@gmail.com>
2025-04-10 21:17:30 +0200

    Rewrite tests by using parametrized test.

--- src/test/java/org/sawaklaudia/sorting/MergeSortTest.java
+++ src/test/java/org/sawaklaudia/sorting/MergeSortTest.java
@@ -2,7 +2,11 @@ package org.sawaklaudia.sorting;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 class MergeSortTest {
 
@@ -13,89 +17,22 @@ class MergeSortTest {
         sortingAlgorithmI = new MergeSort();
     }
 
-    @Test
-    public void shouldReturnEmptyArray() {
-        // given
-        int[] toSort = {};
-
-        // when
-        int[] actual = sortingAlgorithmI.sort(toSort);
-
-        // then
-        Assertions.assertArrayEquals(toSort, actual);
+    private static Stream<Arguments> providedData() {
+        return Stream.of(
+                Arguments.of(new int[]{}, new int[]{}),
+                Arguments.of(new int[]{1}, new int[]{1}),
+                Arguments.of(new int[]{1, 3, 5, 4, 2, 0}, new int[]{0, 1, 2, 3, 4, 5}),
+                Arguments.of(new int[]{0, 1, 2, 3, 4, 5}, new int[]{0, 1, 2, 3, 4, 5}),
+                Arguments.of(new int[]{5, 4, 3, 2, 1, 0}, new int[]{0, 1, 2, 3, 4, 5}),
+                Arguments.of(new int[]{1, 0, 5, 0, 2, 0}, new int[]{0, 0, 0, 1, 2, 5}),
+                Arguments.of(new int[]{-1, -5, -2, -3, -6, -4}, new int[]{-6, -5, -4, -3, -2, -1})
+        );
     }
 
-    @Test
-    public void shouldSortOneElementArray() {
-        // given
-        int[] toSort = {1};
-
-        // when
-        int[] actual = sortingAlgorithmI.sort(toSort);
-
-        // then
-        Assertions.assertArrayEquals(toSort, actual);
-    }
-
-    @Test
-    public void shouldSortUnsortedArray() {
-        // given
-        int[] toSort = {1, 3, 5, 4, 2, 0};
-        int[] expected = {0, 1, 2, 3, 4, 5};
-
-        // when
-        int[] actual = sortingAlgorithmI.sort(toSort);
-
-        // then
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void testAlreadySortedArray() {
-        // given
-        int[] toSort = {0, 1, 2, 3, 4, 5};
-        int[] expected = {0, 1, 2, 3, 4, 5};
-
-        // when
-        int[] actual = sortingAlgorithmI.sort(toSort);
-
-        // then
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void testReversedArray() {
-        // given
-        int[] toSort = {5, 4, 3, 2, 1, 0};
-        int[] expected = {0, 1, 2, 3, 4, 5};
-
-        // when
-        int[] actual = sortingAlgorithmI.sort(toSort);
-
-        // then
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldSortArrayWithDuplicates() {
-        // given
-        int[] toSort = {1, 0, 5, 0, 2, 0};
-        int[] expected = {0, 0, 0, 1, 2, 5};
-
-        // when
-        int[] actual = sortingAlgorithmI.sort(toSort);
-
-        // then
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldSortArrayWithNegativeNumbers() {
-        // given
-        int[] toSort = {-1, -5, -2, -3, -6, -4};
-        int[] expected = {-6, -5, -4, -3, -2, -1};
-
-        // when
+    @ParameterizedTest
+    @MethodSource("providedData")
+    public void shouldSortArray(int[] toSort, int[] expected) {
+        // given when
         int[] actual = sortingAlgorithmI.sort(toSort);
 
         // then
