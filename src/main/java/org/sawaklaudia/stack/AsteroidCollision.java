877357c0fa5a2318fb166dbe50f20faadb906b83
Klaudia <klaud.subs@gmail.com>
2025-06-26 13:07:54 +0200

    Fix moving pointers; Remove zeros from an array

--- src/main/java/org/sawaklaudia/stack/AsteroidCollision.java
+++ src/main/java/org/sawaklaudia/stack/AsteroidCollision.java
@@ -10,36 +10,45 @@ public class AsteroidCollision {
 
         int i = 0;
         int j = 1;
-        while(j < asteroids.length) {
+        while (j < asteroids.length) {
             if (asteroids[i] > 0 && asteroids[j] < 0) { // reduction
                 int sum = asteroids[i] + asteroids[j];
                 if (sum < 0) { // j wins
                     asteroids[i] = 0;
-                } else if(sum > 0) { // i wins
+                } else if (sum > 0) { // i wins
                     asteroids[j] = 0;
                 } else { // both collapse
                     asteroids[i] = 0;
                     asteroids[j] = 0;
                 }
-                j++;
-                if(stack.empty()){
-                    i = j--;
-                } else {
+
+                if (!stack.empty()) {
                     i = stack.pop();
+                } else {
+                    i = j;
+                    j++;
                 }
+            } else if (asteroids[j] == 0) {
+                j++;
             } else {
-                stack.push(asteroids[i]);
-                i++;
+                stack.push(i);
+                i = j;
                 j++;
             }
         }
-        return asteroids;
+
+        return removeZeros(asteroids);
+    }
+
+    private static int[] removeZeros(int[] asteroids) {
+        return Arrays.stream(asteroids)
+                .filter(num -> num != 0)
+                .toArray();
     }
 
     public static void main(String[] args) {
-        int[] asteroids = {10, 2, -5};
-        asteroidCollision(asteroids);
-        System.out.println(Arrays.toString(asteroids));
+        int[] asteroids = {5, 10, -5};
+        int[] result = asteroidCollision(asteroids);
+        System.out.println(Arrays.toString(result));
     }
-
 }
